126794611eb1317c3d38afb6bac646896177ea54
wx1178258 <1178258@qq.com>
2014-06-21 03:54:07 +0800

    Revert "avcodec: add simpleauto idct"
    This reverts commit 08c5859f17ed6c16d7e59ea63c63d85cc0d9427f.

--- libavcodec/arm/dsputil_init_armv5te.c
+++ libavcodec/arm/dsputil_init_armv5te.c
@@ -34,7 +34,6 @@ av_cold void ff_dsputil_init_armv5te(DSPContext *c, AVCodecContext *avctx,
 {
     if (!avctx->lowres && !high_bit_depth &&
         (avctx->idct_algo == FF_IDCT_AUTO ||
-         avctx->idct_algo == FF_IDCT_SIMPLEAUTO ||
          avctx->idct_algo == FF_IDCT_SIMPLEARMV5TE)) {
         c->idct_put              = ff_simple_idct_put_armv5te;
         c->idct_add              = ff_simple_idct_add_armv5te;
